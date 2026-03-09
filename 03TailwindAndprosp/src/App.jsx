89f0b32e32f8ed93699d84d957199deeaef767e9
Vishal Gupta <vishalgupta10852@gmail.com>
2026-03-09 20:16:58 +0530

    Doing some problem on props

---
+++ 03TailwindAndprosp/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-600">Tailwind test</h1>
+      <div className="bg-red-500">click</div>
+    </>
+  );
+}
+
+export default App;
