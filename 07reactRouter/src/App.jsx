acaa7c249aa156c3f6d78e0c72772ee1ac802dfe
Vishal Gupta <vishalgupta10852@gmail.com>
2026-03-19 22:29:24 +0530

    Learning react router topic with project

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="bg-red-300">Router project </h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
