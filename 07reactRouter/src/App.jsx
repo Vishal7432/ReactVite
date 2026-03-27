fb357ea0dc40753a066a592e467e56320e39e5fc
Vishal Gupta <vishalgupta10852@gmail.com>
2026-03-27 16:45:25 +0530

    Completed react Router worrking

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contect from "./components/Contect/Contect";
+import User from "./components/User/User";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contect" element={<Contect />} />
+        <Route path="/user/:userId" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
